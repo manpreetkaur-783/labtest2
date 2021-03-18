6c0f5f196d51422c1f04518ddc3aa8e7af0f3864
manpreetkaur-783 <66217528+manpreetkaur-783@users.noreply.github.com>
2021-03-17 19:37:13 -0500

    Update Screen1.js

--- Screen1.js
+++ Screen1.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
-import {View, Button, TextInput} from 'react'
-export default class Screen extends Component{
+import {View, Button, TextInput} from 'react-native'
+export default class Screen1 extends Component{
     constructor(props) {
-        super(props)
+        super(props);
             this.state={
                 username:''
         };
@@ -15,12 +15,13 @@ export default class Screen extends Component{
             <view >
                 <TextInput placeholder="Enter your Username"
                   
-                   onChangeText={username=>this.setState(username)}>
+                   onChangeText={username=>this.setState({username})}>
 
                    </TextInput>
-
+<TextInput placeholder="enter your password" onChangeText={username=>this.setstate({username})}>
+    </TextInput>
                    <Button title="Enter"
-                   onPress={()=>this.props.navigate('Screen2', {Parameter1:this.state.username})}>
+                   onPress={()=>this.props.navigation.navigate('Screen2', {Parameter1:this.state.username})}>
 
                    </Button>
             </view>
